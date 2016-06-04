a04a2fbfbaf2fc3fc0ead80c0b111e238a709590
anoob <anoob.bava@gmail.com>
2016-06-04 12:38:01 +0530

    added user associations to post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  
+  def my_post?
+    @post.user == current_user ? true : false
+  end
+
+  def comments_any?
+    @post.comments.count > 0 ? true :false
+  end
+
+  def find_my_post_comments
+    @post.comments
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603185138) do
+ActiveRecord::Schema.define(version: 20160604061129) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "description"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160603185138) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
