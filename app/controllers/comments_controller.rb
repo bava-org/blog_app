17ee9471290546fd52d127c6eab9e288d977b79f
anoob <anoob.bava@gmail.com>
2016-06-04 11:39:40 +0530

    added option to delete the comment only if the comentd user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_comment, only: [:destroy]
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
@@ -7,14 +8,26 @@ class CommentsController < ApplicationController
     if @comment.save
      redirect_to @post
     else
-      flash[:notice]= 'Error'
+      flash[:notice]= 'Error in comment creation'
+      render posts_path
     end
   end
 
   def destroy
+    if @comment.destroy
+      redirect_to @post
+    else
+      flash[:notice]= 'error in comment creation'
+      render posts_path
+    end
   end
 
   private
+
+  def find_comment
+    @comment = Comment.find_by_id(params[:id])
+    @post = @comment.post
+  end
 
   def comment_params
     params.require(:comment).permit(:description)
